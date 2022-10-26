a5cc8f60f46065e83ad47f76905af35cbfafee86
Tan Juen Woo <tanjuenwoo@gmail.com>
2022-10-26 22:00:26 +0800

    Update settings.py

--- traceitdb/settings.py
+++ traceitdb/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g4-g65#otu)9e!0egsovo00ui=f0aetkvsi1afb2bj84y0xxx5'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DJANGO_DEBUG') == "True")
 
